10481ed2a810453e7a28b79ea028f1003be252e5
Manu Morante <manu@estadologico.com>
2025-05-25 04:32:07 +0200

    Refactor layout to use Header and Footer components, and update CSS variables for improved color management and styling consistency

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,15 +1,16 @@
 import { Outlet, ScrollRestoration } from 'react-router'
+import { Footer, Header } from '@/components/ui'
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col gap-8">
-      <header>Header</header>
+      <Header />
 
       <main className="main-container flex flex-grow flex-col">
         <Outlet />
       </main>
 
-      <footer>Footer</footer>
+      <Footer />
       <ScrollRestoration />
     </div>
   )
